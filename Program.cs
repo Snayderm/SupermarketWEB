be82748f7b698d3e9dda7071bbe2624e7832c01e
Snayder Martinez Toro <sneyder1002m@gmail.com>
2023-05-07 15:39:11 -0500

    Modelos y configuración del DbContext

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace SupermarketWEB
 			options.UseSqlServer(builder.Configuration.GetConnectionString("SupermarketDB"))
 			);
 
-			var app = builder.Build();
+			var app = builder.Build();  
 
 			// Configure the HTTP request pipeline.
 			if (!app.Environment.IsDevelopment())
