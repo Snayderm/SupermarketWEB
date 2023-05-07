a3663202a0730f2c5ca608554744b49878e6a568
Snayder Martinez Toro <sneyder1002m@gmail.com>
2023-05-07 14:02:47 -0500

    Index Categories

---
+++ Data/SupermarketContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Data
+
+
+{
+	public class SupermarketContext : DbContext
+	{
+		public SupermarketContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categories { get; set; }
+	}
+}
