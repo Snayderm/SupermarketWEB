f3eb395d26a811d6f416a5c52741be45cd2a217f
Snayder Martinez Toro <sneyder1002m@gmail.com>
2023-05-11 00:02:12 -0500

    Index, create Ventas ( se me olvido subir individual)

---
+++ Models/Sale.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketWEB.Models
+{
+    public class Sale 
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Total { get; set; }
+        public string CustomerName { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
+
