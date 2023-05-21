a7a8e85bb9e57a40fb41d2d67f544e9a2e2fa09c
Snayder Martinez Toro <sneyder1002m@gmail.com>
2023-05-21 12:49:47 -0500

    account Login

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,15 @@
-﻿namespace SupermarketWEB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Autenticacion.Model
+
 {
     public class User
     {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
